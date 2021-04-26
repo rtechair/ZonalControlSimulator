79390ea91285c6e51ea961352bba9d3f8e9d31a3
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-04-26 19:46:46 +0200

    Create updateCase.m, function to modify the basecase to obtain the correct one, which is then saved in file 'case6468rte_mod2.mat'

---
+++ updateCase.m
@@ -0,0 +1,77 @@
+function updateCase()
+% update basecase 'case6468rte_mod', this function won't remain as it is in the
+% final version
+% TODO: create flexibility for the update of a basecase
+
+%%Alessio's comment:
+% In order to make the simulations as simple and portable we will use the following framework: 
+% in Matlab the powerflow function (non-linear) within matpower toolbox.
+% In particular, you need to use in matpower, the case 'case6468rte.m'.
+% The zone to be controlled via MPC is the one including the lines between
+% the following buses:  (the letters are indicative, the numbers are important)
+% GR 2076
+% GY 2135
+% MC 2745
+% TR 4720
+% CR 1445
+% Also, you need to add a bus VG in the middle of the line between MC and GR (by dividing by two the impedances)
+% Add the production groups:
+% 78MW at VG
+% 66MW at GR 2076
+% 54MW at MC 2745
+% 10MW at TR 4720
+% A battery of 30Mwh at VG, with power of 10MW both upward and downward.
+% For now, we do not consider the energy of the battery.
+% You need to downsize homogenously the thermal limits up to the point where the line with the highest charge is at 130%.
+% Use a wind production time-line for each production group in the above list.
+% Consider as control lever the fact that we can decrease the production with levels of 25% for each of the busses.
+% The function runpf of matpower should function for the simulation.
+
+%%Update basecase
+
+% load the original case
+mpc=loadcase('case6468rte_mod');
+
+
+% Define the maximum power output for the production groups and the batteries
+PG_VGmax = 78; % 78MW at VG
+PG_GRmax = 66; % 66MW at GR 2076
+PG_MCmax = 54; % 54MW at MC 2745
+PG_TRmax = 10; % 10MW at TR 4720
+PB_VGmax = 10; % 10MW at VG
+PB_VGmin = -10; % -10MW at VG
+
+%% Step 1): Add the desired bus and generators
+% Create a new bus: VG 10000
+addBus(mpc,10000,    2,   0 ,      0,       0 ,  0,   1,  1.03864259,	-11.9454015,	63,	1,	1.07937,	0.952381);
+% Create a new generator at bus VG 10000
+addGenerator(mpc,10000,PG_VGmax,0);
+% Create a  new battery at bus VG 10000
+addGenerator(mpc,10000,PB_VGmax,PB_VGmin);
+% Create a  new generator at bus GR 2076
+addGenerator(mpc,2076,PG_GRmax,0);
+% Create a  new generator at bus MC 2745
+addGenerator(mpc,2745,PG_MCmax,0);
+% Create a  new generator at bus TR 4720
+addGenerator(mpc,4720,PG_TRmax,0);
+
+%% Step 2): Check if there is a line between MC and GR; take the values and create the lines between MC and VG, and VG and GR
+% look for the branch between MC and GR
+fbus = 2745; %MC
+tbus = 2076; %GR
+% there is an unique branch between those 2 buses in this direction
+% there is no branch (2076,fbus) that's why the other direction is not tested
+[branch_idx,~]= find(mpc.branch(:,1)==fbus & mpc.branch(:,2)==tbus); 
+
+branch_info = num2cell(mpc.branch(branch_idx,:)); %https://stackoverflow.com/questions/2337126/how-do-i-do-multiple-assignment-in-matlab;
+[~, ~, r,x, b, rateA, rateB, rateC,ratio,angle,status,angmin,angmax] = branch_info{:};
+
+% remove the branch
+mpc.branch(branch_idx,:) = [];
+% create the new branches between MC and VG, and VG and GR, with half the values of the old branch
+new_bus = 10000;
+mpc.branch(end+1,:) = [fbus, new_bus,r/2,x/2,b/2,rateA,rateB,rateC,ratio,angle,status,angmin,angmax];
+mpc.branch(end+1,:) = [tbus, new_bus,r/2,x/2,b/2,rateA,rateB,rateC,ratio,angle,status,angmin,angmax];
+
+% save the updated basecase
+save('case6468rte_mod2','mpc')
