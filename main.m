aaff78d2fc82c41661c8037be4b481c180fea201
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-04-28 17:12:23 +0200

    Start working on updating the basecase to include zone 2

--- main.m
+++ main.m
@@ -10,72 +10,11 @@ zone2_bus_name = ["VTV" "TRE" "LAZ" "VEY" "SPC" "SIS"];
 %}
 
 %PERSONAL ORDERING, the ascending order has no impact regarding
-%the effectiveness of the code
-zone1_bus = [1445 2076 2135 2745 4720 10000];
-zone1_bus_name = ["CR" "GR" "GY" "MC" "TR" "VG"];
-
-zone2_bus = [2506 4169 4546 4710 4875 4915]';
-zone2_bus_name = ["LAZ" "SIS" "SPC" "TRE" "VTV" "VEY"];
-
-
-
-zone3_bus = [];
-
-zones_bus = {zone1_bus, zone2_bus, zone3_bus};
-
-% Check the basecase is available or computable
-if exist('case6468rte_mod2.mat','file') ~= 2
-    % the file does not exist
-    if exist('case6468rte_mod.m','file') ~= 2
-        % this one neither
-        disp("ERROR: Both case6468rte_mod2.mat and case6468rte_mod.mat are missing, can't do anything with no basecase")
-    else
-        % compute it using the 1st one
-        updateCase()
-    end
-end
-
-basecase = loadcase('case6468rte_mod2'); %mpc_ext
-
-%%MAP
-% get a map of bus id to bus index for basecase.bus
-mapBus_id2idx = getMapBus_id2idx(basecase);
-% while idx2id is immediate using basecase.bus
-mapBus_idx2id = containers.Map(1:size(basecase.bus,1),basecase.bus(:,1));
-
-%%CONVERSION OF ZONE 1
-% get the list of indices corresponding to where zone1_bus buses are
-% recall zone1_bus = [2076 2135 2745 4720  1445 10000]'
-zone1_bus_idx = cell2mat(values(mapBus_id2idx, num2cell(zone1_bus))); % [2076;2135;2743;4717;1445;6469]
-
-%this get the matrix corresponding to the subset of basecase.bus of zone 1
-corresponding_zone1_bus_idx = basecase.bus(zone1_bus_idx,:);
-
-
-basecase_int = ext2int(basecase);
-
-% NOT WORKING zone1_bus_int = e2i_data(basecase_int, corresponding_zone1_bus_idx, 'bus');
-
-mapBus_ext2int = basecase_int.order.bus.e2i; % 10000x1 sparse double
-mapBus_int2ext = basecase_int.order.bus.i2e; % 6469x1 double
-
-zone1_bus_int_idx = mapBus_ext2int(zone1_bus_idx,1); % [1445;2076;2135;2741;4714;6462]
-zone1_bus_back_idx = mapBus_int2ext(zone1_bus_int_idx); % [1445;2076;2135;2743;4717;6469]
-
-zone1_bus_back_id = cell2mat(values(mapBus_idx2id,num2cell(zone1_bus_back_idx))); % [1445;2076;2135;2745;4720;10000]
-
-% TODO: investigate why when zone1_bus is a row vector, some resulting
-% variables remain column vectors and not all variables become row vectors
-% too
-
-
-%% CONVERSION OF ZONE 2
-% recall zone2_bus = [2506 4169 4546 4710 4875 4915]'
-zone2_bus_idx = cell2mat(values(mapBus_id2idx, num2cell(zone2_bus))); % [2505;4167;4543;4707;4872;4912]
-zone2_bus_int_idx = mapBus_ext2int(zone2_bus_idx,1); % [2504;4165;4540;4704;4869;4909]
-zone2_bus_back_idx = mapBus_int2ext(zone2_bus_int_idx); % [2505;4167;4543;4707;4872;4912]
-zone2_bus_back_id = cell2mat(values(mapBus_idx2id,num2cell(zone2_bus_back_idx))); % [2506;4169;4546;4710;4875;4915]
-
+%the effectiveness of the code. 
+% However, selection of column vector instead of row
+%vector is meant for consistency with column vectors obtained using
+%MatPower functions 
+% https://matpower.org/docs/ref/
 
 
 %% HOW CONVERSION WORKS
@@ -90,9 +29,66 @@ bus_idx     using i2e
 bus_id      using idx2id
 %}
 
-% https://matpower.org/docs/ref/matpower7.1/lib/e2i_data.html
-% https://matpower.org/docs/ref/matpower7.1/lib/e2i_field.html
-% https://matpower.org/docs/ref/
+zone1_bus = [1445 2076 2135 2745 4720 10000]';
+zone1_bus_name = ["CR" "GR" "GY" "MC" "TR" "VG"];
+
+zone2_bus = [2506 4169 4546 4710 4875 4915]';
+zone2_bus_name = ["LAZ" "SIS" "SPC" "TRE" "VTV" "VEY"];
+
+
+zone3_bus = [];
+
+zones_bus = {zone1_bus, zone2_bus, zone3_bus};
+
+%% BASECASE
+
+handleBasecase();
+
+basecase = loadcase('case6468rte_mod2'); %mpc_ext
+
+
+
+%% MAP
+% get a map of bus id to bus index for basecase.bus
+mapBus_id2idx = getMapBus_id2idx(basecase);
+% while idx2id is immediate using basecase.bus
+mapBus_idx2id = containers.Map(1:size(basecase.bus,1),basecase.bus(:,1));
+
+%%CONVERSION OF ZONE 1
+% get the list of indices corresponding to where zone1_bus buses are
+% recall zone1_bus =                                                  [2076 2135 2745 4720  1445 10000]'
+zone1_bus_idx = getValues(mapBus_id2idx, zone1_bus);                % [2076;2135;2743;4717;1445;6469]
+
+
+%this get the matrix corresponding to the subset of basecase.bus of zone 1
+corresponding_zone1_bus_idx = basecase.bus(zone1_bus_idx,:);
+
+
+basecase_int = ext2int(basecase);
+
+
+mapBus_ext2int = basecase_int.order.bus.e2i; % 10000x1 sparse double
+mapBus_int2ext = basecase_int.order.bus.i2e; % 6469x1 double
+
+zone1_bus_int_idx = mapBus_ext2int(zone1_bus_idx,1);                % [1445;2076;2135;2741;4714;6462]
+zone1_bus_back_idx = mapBus_int2ext(zone1_bus_int_idx);             % [1445;2076;2135;2743;4717;6469]
+zone1_bus_back_id = getValues(mapBus_idx2id,zone1_bus_back_idx);    % [1445;2076;2135;2745;4720;10000]
+
+% TODO: investigate why when zone1_bus is a row vector, some resulting
+% variables remain column vectors and not all variables become row vectors
+% too
+
+
+%% CONVERSION OF ZONE 2
+% recall zone2_bus =                                                  [2506 4169 4546 4710 4875 4915]'
+zone2_bus_idx = getValues(mapBus_id2idx, zone2_bus);                % [2505;4167;4543;4707;4872;4912]
+zone2_bus_int_idx = mapBus_ext2int(zone2_bus_idx,1);                % [2504;4165;4540;4704;4869;4909]
+zone2_bus_back_idx = mapBus_int2ext(zone2_bus_int_idx);             % [2505;4167;4543;4707;4872;4912]
+zone2_bus_back_id = getValues(mapBus_idx2id,zone2_bus_back_idx);    % [2506;4169;4546;4710;4875;4915]
+
+
+
+
 
 
 
@@ -102,3 +98,26 @@ a = 1;
 
 
 % creating class: https://www.mathworks.com/help/matlab/matlab_oop/create-a-simple-class.html
+
+
+function handleBasecase()
+    b1 = exist('case6468rte_mod.m','file') == 2; % initial basecase provided by Jean
+    b2 = exist('case6468rte_mod2.mat','file') == 2; % updated basecase for zone 1
+    b3 = exist('case6468rte_mod3.mat','file') == 2; % updated basecase for zone 1 & 2
+    b_tot = 4*b3 + 2*b2 + b1;
+    switch b_tot
+        % basecase includes zone 1 and zone 2
+        case {4, 5, 6, 7}
+            return
+        % basecase includes zone 1 but not zone 2
+        case {2, 3}
+            % TODO
+            return
+        % basecase does not include zone 1 nor zone 2, but the initial basecase
+        case {1}
+            % so compute the updated basecase
+            updateCaseForZone1();
+        otherwise
+            disp("ERROR: Both case6468rte_mod2.mat and case6468rte_mod.mat are missing, can't do anything with no basecase")
+    end
+end
