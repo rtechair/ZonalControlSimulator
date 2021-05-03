e987a852968fc69b95ad863fec5fe8460161b75d
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-05-03 20:37:52 +0200

    the mappings of buses from id to index and vice versa are obtained from the same function

--- getMapBus_id2idx_idx2id.m
+++ getMapBus_id2idx_idx2id.m
@@ -1,13 +1,15 @@
-function [mapBus_id2idx] = getMapBus_id2idx(basecase)
-    % basecase.bus matrix gives bus_id by providing bus_idx, but it is
-    % necessary to access data the other way around, i.e. providing bus_id to
-    % obtain bus_idx
+function [mapBus_id2idx, mapBus_idx2id] = getMapBus_id2idx_idx2id(basecase)
+    % Return map giving the index in the basecase.bus matrix of a given bus
+    % id, and vice versa
     %% INPUT
     % basecase: a basecase with a MatPower case struct
     %% OUTPUT
     % mapBus_id2idx: a map with key: bus id, and value: bus index in the
     % basecase.bus
+    % mapBus_idx2id: a map with key = bus index, and value = bus id
     id = basecase.bus(:,1);
     idx = 1:size(basecase.bus,1); % = [1,2,...,numberOfBuses]
     mapBus_id2idx = containers.Map(id,idx);
+    mapBus_idx2id = containers.Map(idx,id);
 end
+
