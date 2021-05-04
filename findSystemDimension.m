a8f6a0e370b9d09cb30e7f6ec0286f368a4d660d
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-05-04 13:19:52 +0200

    Get the dimension of a matpower struct, i.e. a basecase, by using the dedicated function findSystemDimension

---
+++ findSystemDimension.m
@@ -0,0 +1,25 @@
+function [nb_bus, nb_branch, nb_gen, nb_batt] = findSystemDimension(basecase)
+    % Show the dimensions of the considered matpower case
+    %% Input
+    % case: matpower case
+    %% Output:
+    % 'nb_bus': number of buses
+    % 'nb_branch': number of branches
+    % 'nb_gen': number of exclusive generators, no battery included
+    % 'nb_battery': number of batteries
+    
+    % number of buses
+    nb_bus = size(basecase.bus,1);
+    % number of branches
+    nb_branch = size(basecase.branch,1);
+    
+    % new version
+    Pg_min = basecase.gen(:,10);
+    % a generator is in fact a battery if Pg_min < 0
+    isItaBattery = Pg_min < 0; 
+    nb_batt = sum(isItaBattery);
+    % As both exclusive gen and batteries are contained in the basecase.gen
+    % thus, nb_gen = (nb_gen + nb_batt) - nb_batt
+    nb_gen = size(basecase.gen,1) - nb_batt;
+end
+    
