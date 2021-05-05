1174b94a043388b74091ca2c4bbefebdf7f3e334
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-05-05 12:05:02 +0200

    The matrix A is constructed

--- fromMatpowerToABD.m
+++ fromMatpowerToABD.m
@@ -1,11 +1,29 @@
-function [A,Bc,Bd,Dg,Dn,Da,x,u,d] = fromMatpowerToABD(basecase_int, zone_bus, zone_branch_inner_idx,zone_gen_idx, zone_battery_idx)
+function [A,Bc,Bd,Dg,Dn,Da,x,u,d] = fromMatpowerToABD(basecase_int, zone_bus,...
+    zone_branch_inner_idx,zone_gen_idx, zone_battery_idx, ...
+    sampling_time, batt_cst_power_reduc, n_time_steps)
 % important: this concerns only one zone here
+%% Input
+% batt_cst_power_reduc : must be a vector
 %{ 
 Matrices definition for the linear system x(k+1)=Ax(k)+Bu(k+tau)+Dd(k)
 x = [Fij Pc Pb Eb Pg ]     uc = DeltaPc      ub =DeltaPb     
 w = DeltaPg      h = DeltaPT
 The model is described by the equations x(k+1) = A*x(k) + Bc*DPC(k-tau_c) + Bb*DPB(k-tau_b) + Dg*DPG(k) + Dn*DPT(k) + Da*DPA(k)
 %}
+
+arguments
+    basecase_int
+    zone_bus
+    zone_branch_inner_idx
+    zone_gen_idx
+    zone_battery_idx
+
+    sampling_time (1,1) double
+    batt_cst_power_reduc %must be a vector
+    n_time_steps (1,1) double = 5
+end
+    
+
 
 ISF = makePTDF(basecase_int);
 
@@ -19,12 +37,12 @@ ISF = makePTDF(basecase_int);
 n_state_variables = n_branch + 2*n_gen + 2*n_battery;
 n_time_steps = 5; %TODO check this is indeed the nb_of_time_steps, can it be an input?
 
-% A = zeros(n_state_variables);
+
 Bc = zeros(n_state_variables, n_gen);
 
 % depending on if there are some batteries within the zone or not
 if n_battery == 0
-    Bb = 0;
+    Bb = 0; %TODO maybe this special case is not needed
 else
     Bb = zeros(n_state_variables);
 end
@@ -42,8 +60,9 @@ d = zeros(n_bus, n_time_steps); %TODO check it is correct
 %% Matrix element definition
 %{
  In the following:
-tmp_idx serves as temporary indices to construct the submatrices that are modified
-tmp_start indicates what is the initial starting row and column, thus cell, of the submatrices
+'tmp_range_row' and 'tmp_range_col' serve as a temporary ranges of indices to
+edit submatrices values
+'tmp_start_row' and 'tmp_start_col' indicates what is the initial starting row and column, thus cell, of the submatrices
 %}
 
 % 1) generate the coefficients for the A matrix w.r.t.:
@@ -56,28 +75,23 @@ Pg(k+1) += Pg(k)
 %}
 A = eyes(n_state_variables);
 
-%{
-USELESS NOW AS THIS IS REPLACED BY WHAT COMES BEFORE
-% w.r.t. Fij(k+1) += Fij(k)
-tmp_idx = 1:n_branch;
-A(tmp_idx,tmp_idx) = eye(n_branch);
+% Eb(k+1) -= T*cb*Pb(k)
+tmp_start_row = n_branch + n_gen + n_battery + 1;
+tmp_start_col = n_branch + n_gen + 1;
+tmp_range_row = tmp_start_row : tmp_start_row + n_battery - 1;
+tmp_range_col = tmp_start_col : tmp_start_col + n_battery - 1;
 
-% w.r.t. Pc(k+1) += Pc(k)
-tmp_start = n_branch + 1;
-tmp_idx = tmp_start: tmp_start+n_gen-1; % if n_gen = 0, then A(I,I) = [], so no modification on A. No need to handle special case n_gen = 0
-A(tmp_idx,tmp_idx) = eye(n_gen);
+A(tmp_range_row, tmp_range_col) = - sampling_tim*diag(batt_cst_power_reduc);
+% notice with the previous operation, if there is no battery, then the
+% submatrix to be modified is a empty double matrix which does not modify the matrix, so no special case to
+% handle
 
-% w.r.t. Pb(k+1) += Pb(k)
-tmp_start = n_branch + n_gen + 1
-tmp_idx = tmp_start : tmp_start + n_battery - 1
-% w.r.t. Eb(k+1) += Eb(k)
-%}
+% 2) Generate the coefficients for the Bc matrix w.r.t.
+% Pc(k+1) += DeltaPc(k-tau)
 
-% Eb(k+1) += Eb(k+1)
+Bc =
 
+% Pg(k+1) -= DeltaPc(k-tau)
 
-
-
-
-
+% F(k+1) -= diag(ptdf)*DeltaPc(k-tau)
 
