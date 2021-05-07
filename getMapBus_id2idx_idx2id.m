d273f5121ddda259d211e1d9d743710ebcea3af6
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-05-07 12:23:13 +0200

    maps of bus: id2idx and idx2id are no more containers.Map but now are sparse matrices. This should avoid the necessity of functions to handle containers.Map

--- getMapBus_id2idx_idx2id.m
+++ getMapBus_id2idx_idx2id.m
@@ -1,16 +1,19 @@
 function [mapBus_id2idx, mapBus_idx2id] = getMapBus_id2idx_idx2id(basecase)
-    % Return map giving the index in the basecase.bus matrix of a given bus
+    % Return sparse matrices serving as map giving the index in the basecase.bus matrix of a given bus
     % id, and vice versa
     %% INPUT
     % basecase: a basecase with a MatPower case struct
     %% OUTPUT
-    % mapBus_id2idx: a map with key: bus id, and value: bus index in the
-    % basecase.bus
-    % mapBus_idx2id: a map with key = bus index, and value = bus id
+    % mapBus_id2idx: a sparse matrix serving as a map: bus_id -> bus_idx
+    % where bus_id and bus_idx are the identity and index of the bus
+    % respectively in the basecase.bus
+    % mapBus_idx2id: a sparse matrix serving as a map: bus_idx -> bus_id
+    n_bus = size(basecase.bus,1);
     id = basecase.bus(:,1);
-    idx = 1:size(basecase.bus,1); % = [1,2,...,numberOfBuses]
+    idx = 1:n_bus;
+    col = ones(n_bus, 1);
     % construct maps in both directions
-    mapBus_id2idx = containers.Map(id,idx);
-    mapBus_idx2id = containers.Map(idx,id);
+    mapBus_id2idx = sparse(id, col, idx);
+    mapBus_idx2id = sparse(idx, col, id);
 end
 
