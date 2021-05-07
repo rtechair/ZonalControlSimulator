c885e7c2012cc53445df1c003fd270f766e7b311
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-05-07 14:24:09 +0200

    modification of the bus maps

--- getMapBus_id2idx_idx2id.m
+++ getMapBus_id2idx_idx2id.m
@@ -1,15 +1,15 @@
 function [mapBus_id2idx, mapBus_idx2id] = getMapBus_id2idx_idx2id(basecase)
-    % Return 2 matrices: 
-    % 1) a sparse matrix serving as a map for buses from id to index in the
+    % Return 2 matrices serving as maps bus_id <-> bus_idx for the external basecase: 
+    % 1) a sparse matrix serving as a bus map from id to index in the
     % basecase.bus
-    % 2) inversely, the map for buses from index to id is returned as continuous indexing matrix
+    % 2) inversely, a continuous indexing matrix serves as a bus map from
+    % index to id in the basecase.bus
     %% INPUT
     % basecase: a basecase with a MatPower case struct
     %% OUTPUT
-    % mapBus_id2idx: a sparse matrix serving as a map: bus_id -> bus_idx
-    % where bus_id and bus_idx are the identity and index of the bus
-    % respectively in the basecase.bus
-    % mapBus_idx2id: a matrix with continuous indexing serving as a map: bus_idx -> bus_id
+    % mapBus_id2idx: identity -> index sparse matrix of basecase.bus
+    % mapBus_idx2id: index -> identity matrix, with continuous indexing, of basecase.bus
+    
     n_bus = size(basecase.bus,1);
     id = basecase.bus(:,1);
     idx = 1:n_bus;
--- getMapBus_id_e2i_i2e.m
+++ getMapBus_id_e2i_i2e.m
@@ -1,18 +1,19 @@
 function [mapBus_id_e2i, mapBus_id_i2e] = getMapBus_id_e2i_i2e(basecase_int, mapBus_id2idx, mapBus_idx2id, mapBus_idx_e2i, mapBus_idx_i2e)
-% Return 2 maps: one to obtain interior buses id from their exterior bus
-% id, and vice versa, respectively
+% Return 2 matrices serving as maps, for the bus id, exterior <-> interior basecase:
+% 1) a sparse matrix serving as a map for bus id, from the external to the internal basecase
+% 2) inversely, a continuous indexing matrix serves as a map for bus id, from the internal to the external basecase
 %% Input
 % basecase_int: the internal basecase
 %% Output
-% mapBus_id_e2i: the Exterior -> Interior bus_id Map
-% mapBus_id_i2e: the Interior -> Exterior bus_id map
+% mapBus_id_e2i: exterior -> interior bus_id sparse matrix
+% mapBus_id_i2e: interior -> exterior bus_id matrix with continuous indexing
 
 % get the keys and values to create the maps
 bus_id = basecase_int.order.ext.bus(:,1);
 bus_idx = 1:size(bus_id);
 % bus_idx = getValues(mapBus_id2idx, bus_id);  % TODO check function works before deleting              
 bus_int_idx = mapBus_idx_e2i(bus_idx); % TODO check function works before deleting  
-bus_int_idx2 = nonzeros(mapBus_idx_e2i);
+
 % create the maps:
 mapBus_id_e2i = containers.Map(bus_id,bus_int_idx);
 mapBus_id_i2e = containers.Map(bus_int_idx, bus_id);
