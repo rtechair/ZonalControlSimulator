9d2e9c5adc9fa1b4b4cc788e70444a6b6576a8a3
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-05-11 10:56:02 +0200

    Creating Zone class, such that the properties of a zone are stored inside the associate object of class Zone

--- Zone.m
+++ Zone.m
@@ -1,48 +1,45 @@
 classdef Zone
     properties
-        % general elements
-        basecase
-        basecase_int % MatPower vision
-        mapBus_id2idx
-        mapBus_idx2id
-        mapBus_ext2int
-        mapBus_int2ext
-        % elements of the zone
-        bus_id
-        bus_idx
-        bus_int_idx
-        mapBus_id2idxZone
-        mapBus_idxZone2id
-        branch_idx
-        branch_int_idx
+        % the properties starts a majuscule only because the following tutorial
+        % does the same way: https://www.mathworks.com/help/matlab/matlab_oop/example-representing-structured-data.html
+        
+        % elements of the inner zone
+        Bus_id
+        Bus_idx
+        Bus_int_id
+
+        Branch_idx
+        % branch_int_idx % if no branch deleted, then branch_int_idx =
+        % branch_idx, thus unnecessary
+        
+        GenOn_idx
+        BattOn_idx
+        GenOn_int_idx
+        BattOn_int_idx
         
         % peripheral / nearby zone
-        bus_near_id
-        bus_near_idx
-        bus_near_int_idx
-        
-        branch_inner_id
-        branch_inner_idx
-        branch_near_id
-        
-        gen_inner
+        Bus_border_id
+        Branch_border_idx
+
     end
     
     methods
-        function obj= Zone(bus_id, basecase, basecase_int, mapBus_id2idx, mapBus_idx2id)
-            obj.bus_id = bus_id;
-            obj.basecase = basecase;
-            obj.basecase_int = basecase_int;
-            obj.mapBus_id2idx = mapBus_id2idx;
-            obj.mapBus_idx2id = mapBus_idx2id;
-            obj.mapBus_ext2int = basecase_int.order.bus.e2i;
-            obj.mapBus_int2ext = basecase_int.order.bus.i2e;
-            
-            obj.bus_idx = getValues(obj.mapBus_id2idx, obj.bus_id);
-            obj.bus_int_idx = obj.mapBus_ext2int(obj.bus_idx,1);
+        function obj= Zone(bus_id, basecase_int)
+            % create the zone, providing the buses, branches, gen and batt of
+            % the zone, alongside the buses and branches at the border of
+            % the zone.
+            obj.Bus_id = bus_id;
+            basecase = basecase_int.order.ext;
+            [obj.Branch_idx, obj.Branch_border_idx] = findInnerAndBorderBranch(bus_id, basecase);
+            obj.Bus_border_id = findBorderBus(bus_id, obj.Branch_border_idx, basecase);
+            [obj.GenOn_idx, obj.BattOn_idx] = findGenAndBattOnInZone(bus_id, basecase);
         end
         
-        %function obj = identifyBranch(obj)
+        function obj = SetInteriorProperties(obj, mapBus_id_e2i, mapGenOn_idx_e2i)
+            obj.Bus_int_id = mapBus_id_e2i(obj.Bus_id);
+            obj.GenOn_int_idx = mapGenOn_idx_e2i(obj.GenOn_idx);
+            obj.BattOn_int_idx = mapGenOn_idx_e2i(obj.BattOn_idx);
+        end
             
     end
 end
@@ -53,6 +50,9 @@ https://www.mathworks.com/help/matlab/matlab_oop/create-a-simple-class.html
 https://www.mathworks.com/help/matlab/matlab_oop/specifying-methods-and-functions.html
 https://www.mathworks.com/help/matlab/matlab_oop/properties.html
 https://www.mathworks.com/help/matlab/ref/classdef.html
+
+https://www.mathworks.com/matlabcentral/answers/350158-convert-a-struct-to-an-object-of-a-class
+https://www.mathworks.com/help/matlab/matlab_oop/example-representing-structured-data.html
 %}
 
 %{
