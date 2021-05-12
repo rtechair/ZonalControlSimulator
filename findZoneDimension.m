dba3bd349ef29d3fa7ceeb54a843ca12be3691cf
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-05-12 16:49:37 +0200

    Update names of output variables of findZoneDimension for clarity

--- findZoneDimension.m
+++ findZoneDimension.m
@@ -1,6 +1,6 @@
-function [nb_bus, nb_branch, nb_gen, nb_batt] = findZoneDimension(bus, branch, gen, batt)
-    nb_bus = size(bus,1);
-    nb_branch = size(branch,1);
-    nb_gen = size(gen,1);
-    nb_batt = size(batt,1);
+function [n_bus, n_branch, n_gen, n_batt] = findZoneDimension(bus, branch, gen, batt)
+    n_bus = size(bus,1);
+    n_branch = size(branch,1);
+    n_gen = size(gen,1);
+    n_batt = size(batt,1);
 end
