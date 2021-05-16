56b9abd5318e9427451ce80230d548f1fba38ec1
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-05-16 22:45:11 +0200

    Modify the main file to reflect the improvements made on the project

--- main.m
+++ main.m
@@ -49,7 +49,6 @@ basecase_int = ext2int(basecase);
 % check if a bus or a branch has been deleted, currently the code does not
 % handle the case if some are deleted/off
 [isBusDeleted, isBranchDeleted] = isBusOrBranchDeleted(basecase_int);
-
 if isBusDeleted
     disp('a bus has been deleted, nothing has been made to handle this situation, the code should not work')
 end
@@ -111,8 +110,9 @@ zone1_bus_border_id = findBorderBus(zone1_bus_id, zone1_branch_border_idx, basec
 [zone1_gen_idx, zone1_battery_idx] = findGenAndBattOnInZone(zone1_bus_id, basecase);
 %}
 
-z1 = Zone(zone1_bus_id, basecase_int);
-z2 = Zone(zone2_bus_id, basecase_int);
+
+z1 = Zone(basecase, zone1_bus_id);
+z2 = Zone(basecase, zone2_bus_id);
 
 z1 = setInteriorIdAndIdx( z1, mapBus_id_e2i, mapGenOn_idx_e2i);
 z2 = setInteriorIdAndIdx( z2, mapBus_id_e2i, mapGenOn_idx_e2i);
@@ -136,9 +136,10 @@ z1.Simulation_time_unit = 1; % sec, TODO look at the previous main, Sampling_tim
 z1_cb = 0.001; % conversion factor for battery power output
 z1.Batt_cst_power_reduc = z1_cb * ones(z1.N_battOn,1); % TODO: needs to be changed afterwards, with each battery coef
 
-
+%{
  z1 = setDynamicSystem(z1, basecase_int, z1.Bus_id, z1.Branch_idx, z1.GenOn_idx, z1.BattOn_idx,...
                 mapBus_id_e2i, mapGenOn_idx_e2i, z1.Simulation_time_unit, z1.Batt_cst_power_reduc);
+%}
 %{
  [z1.A, z1.Bc, z1.Bb, z1.Dg, z1.Dt, z1.Da] = dynamicSystem(basecase_int, z1.Bus_id, z1.Branch_idx, z1.GenOn_idx, z1.BattOn_idx,...
                mapBus_id_e2i, mapGenOn_idx_e2i, z1.Simulation_time_unit, z1.Batt_cst_power_reduc);
@@ -151,9 +152,10 @@ z2.Simulation_time_unit = 1; % sec, TODO look at the previous main, Sampling_tim
 z2_cb = 0.001; % conversion factor for battery power output
 z2.Batt_cst_power_reduc = z2_cb * ones(z2.N_battOn,1); % TODO: needs to be changed afterwards, with each battery coef
 
+%{
 z2 = setDynamicSystem(z2, basecase_int, z2.Bus_id, z2.Branch_idx, z2.GenOn_idx, z2.BattOn_idx,...
                 mapBus_id_e2i, mapGenOn_idx_e2i, z2.Simulation_time_unit, z2.Batt_cst_power_reduc);
-
+%}
 
 %% Simulation initialization
 
@@ -163,15 +165,24 @@ duration = 600;
 z1.Sampling_time = 5;
 
 z1.N_iteration = floor(duration / z1.Sampling_time);
-z1 = getPAandDeltaPA(z1, 'tauxDeChargeMTJLMA2juillet2018.txt',basecase, duration);
+z1 = getPAandDeltaPA(z1, basecase, 'tauxDeChargeMTJLMA2juillet2018.txt');
 
 z2.Sampling_time = 5;
-z2 = getPAandDeltaPA(z2, 'tauxDeChargeMTJLMA2juillet2018.txt',basecase, duration);
+z2.N_iteration = floor(duration / z2.Sampling_time);
+z2 = getPAandDeltaPA(z2, basecase, 'tauxDeChargeMTJLMA2juillet2018.txt');
 
 
-%% Set some preconfigurated curtailment
+%% Set some preconfigurated curtailment DeltaPC
+z1_maxPA_of_genOn = basecase.gen(z1.GenOn_idx, 9); % computed inside getPAandDeltaPA, but can't be in setDeltaPC as basecase not provided
+z1 = setDeltaPC(z1, [1/7 1/3 2/3], 0.2, z1_maxPA_of_genOn); 
 
+z2_maxPA_of_genOn = basecase.gen(z2.GenOn_idx, 9);
+z2 = setDeltaPC(z2, [1/7 1/3 2/3], 0.2, z2_maxPA_of_genOn);
 
+%% Compute PC
 
+z1 = setPC(z1);
+z2 = setPC(z2);
 
-
+%% Compute PB
+% DeltaPB has not been set however
