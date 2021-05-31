b00e9cc7afc731be055af02bcc259a8fdaf6ce9c
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-05-31 16:12:38 +0200

    in the class Zone, change the name of several properties. Additionally, define some set property functions to handle the creation of properties dependent on other properties

--- Zone.m
+++ Zone.m
@@ -1,40 +1,63 @@
 classdef Zone < matlab.mixin.Copyable
     % matlab.mixin.Copyable
     % https://www.mathworks.com/help/matlab/matlab_oop/custom-copy-behavior.html
-    % Zone < handle
     % Now the class inherits 'handle', so it is a handle class and not a
     % value class
     properties
-        % the properties starts a majuscule only because the following tutorial
-        % does the same way: https://www.mathworks.com/help/matlab/matlab_oop/example-representing-structured-data.html
+        % By convention on Matlab, properties start with a capital letter
+        %{
+        The basecase can be seen as an external one, matpower creates an
+        internal basecase based on the external basecase:
+        - If necessary, bus are reordered in increasing consecutive order
+        - generators off, including batteries off, are not conserved in the
+        internal basecase
+        - "islands", i.e. isolated buses, are not conserved in the internal
+        basecase, and adjacent branches neither. No such bus or branch are
+        deleted for the Rte basecases, thus handling this peculiar
+        situation has not been done. If such a situation happens, nothing
+        in the code has been done in order to handle it.
+        %}
         
-        % elements of the inner zone
-        Bus_id (:,1) {mustBeInteger}
-        Bus_idx (:,1) {mustBeInteger}
-        Bus_int_id (:,1) {mustBeInteger}
-
-        Branch_idx (:,1) {mustBeInteger}
-        % if no branch deleted, then branch_int_idx =
-        % branch_idx, thus unnecessary
+        %% elements of the inner zone
+        BusId (:,1) {mustBeInteger}
+        BusIdx (:,1) {mustBeInteger}
+        BusIntId (:,1) {mustBeInteger}
+        BranchIdx (:,1) {mustBeInteger}
         
-        GenOn_idx (:,1) {mustBeInteger}
-        BattOn_idx (:,1) {mustBeInteger}
-        GenOn_int_idx (:,1) {mustBeInteger}
-        BattOn_int_idx (:,1) {mustBeInteger}
+        %{
+        BranchIntIdx ? No need for it:
+        if no branch deleted, then branchIntIdx = BranchIdx, thus unnecessary. 
+        However, in case some branches are deleted due to the selection of a different basecase,
+        then the whole code is incorrect. All basecase instances from Rte have been checked
+        that no branch is deleted
+        %}
         
-        % peripheral / nearby zone
-        Bus_border_id (:,1) {mustBeInteger}
-        Branch_border_idx (:,1) {mustBeInteger}
         
-        % Number of elements
-        N_bus (1,1) {mustBeInteger}
-        N_branch (1,1) {mustBeInteger}
-        N_genOn (1,1) {mustBeInteger}
-        N_battOn (1,1) {mustBeInteger}
+        %{
+        Generators and batteries do not have an id, their index is used as such.
+        Batteries are treated as generators with negative value for minimal power injection in the matpower case.
         
-        N_iteration (1,1) {mustBeInteger}
+        during the internal basecase conversion by Matpower using
+        'ext2int', all generators Off and batteries Off are deleted and
+        does not appear in the internal basecase. Thus the specification of
+        ON generators and batteries.
+        %}
+        GenOnIdx (:,1) {mustBeInteger}
+        BattOnIdx (:,1) {mustBeInteger}
+        GenOnIntIdx (:,1) {mustBeInteger}
+        BattOnIntIdx (:,1) {mustBeInteger}
         
-        %Dynamic Model Operator
+        %% border / nearby zone
+        BusBorderId (:,1) {mustBeInteger}
+        BranchBorderIdx (:,1) {mustBeInteger}
+        
+        %% Number of elements
+        NumberBus (1,1) {mustBeInteger}
+        NumberBranch (1,1) {mustBeInteger}
+        NumberGenOn (1,1) {mustBeInteger}
+        NumberBattOn (1,1) {mustBeInteger}
+             
+        %% Dynamic Model Operator
         A   % state
         Bc  % control curtailment
         Bb  % control battery
@@ -56,32 +79,42 @@ classdef Zone < matlab.mixin.Copyable
         Fij
         EB
         
-        % Other
-        Sampling_time (1,1) {mustBeInteger, mustBeNonempty}
-        Simulation_time_unit (1,1) {mustBeInteger, mustBeNonempty}
-        Batt_cst_power_reduc (1,1) {mustBeNonempty}
-        Delay_batt (1,1) {mustBeInteger, mustBeNonempty}
-        Delay_curt (1,1) {mustBeInteger, mustBeNonempty}
-        maxPG (:,1)
+        %% Other
+        SamplingTime (1,1) {mustBeInteger, mustBeNonempty} = 5 
+        % 'SamplingTime' default value  is necessary for function 'updateNumberIteration'
+        SimulationTimeUnit (1,1) {mustBeInteger, mustBeNonempty}
+        BattConstPowerReduc (1,1) {mustBeNonempty}
+        
+        DelayBattSec (1,1) {mustBeInteger}
+        DelayCurtSec (1,1) {mustBeInteger}
+        
+        MaxPG (:,1)
+        Duration (1,1) {mustBeInteger}
+    end
+    
+    properties (SetAccess = private)
+        NumberIteration (1,1) {mustBeInteger}
+        DelayBatt (1,1) {mustBeInteger, mustBeNonempty}
+        DelayCurt (1,1) {mustBeInteger, mustBeNonempty}      
     end
     
     methods
-        function zone = Zone(basecase, bus_id)
+        function obj = Zone(basecase, busId)
             % create the zone, providing the buses, branches, gen and batt ON of
             % the zone, alongside the buses and branches at the border of
             % the zone
             arguments
                 basecase struct % MatPower Case struct
-                bus_id (:,1) {mustBeInteger, mustBeNonempty, mustBusBeFromBasecase(bus_id, basecase)}
+                busId (:,1) {mustBeInteger, mustBeNonempty, mustBusBeFromBasecase(busId, basecase)}
             end
             % Define the bus id, branch indices, generators and batteries On within the zone.            
             % plus bus and branches at the border of the zone
-            zone.Bus_id = bus_id;
-            [zone.Branch_idx, zone.Branch_border_idx] = findInnerAndBorderBranch(basecase, bus_id);
-            zone.Bus_border_id = findBorderBus(basecase, bus_id, zone.Branch_border_idx);
-            [zone.GenOn_idx, zone.BattOn_idx] = findGenAndBattOnInZone(bus_id, basecase);
+            obj.BusId = busId;
+            [obj.BranchIdx, obj.BranchBorderIdx] = findInnerAndBorderBranch(basecase, busId);
+            obj.BusBorderId = findBorderBus(basecase, busId, obj.BranchBorderIdx);
+            [obj.GenOnIdx, obj.BattOnIdx] = findGenAndBattOnInZone(busId, basecase);
             
-            zone = setNumberOfElements(zone);
+            obj.updateNumberElement;
         end
         
         function obj = setInteriorIdAndIdx(obj, mapBus_id_e2i, mapGenOn_idx_e2i)
@@ -97,9 +130,9 @@ classdef Zone < matlab.mixin.Copyable
             % because the maps are sparse matrix, the returned column vector is
             % also sparsed, hence the use of 'full' function to obtain a
             % full column vector
-            obj.Bus_int_id = full(mapBus_id_e2i(obj.Bus_id)); 
-            obj.GenOn_int_idx = full(mapGenOn_idx_e2i(obj.GenOn_idx));
-            obj.BattOn_int_idx = full(mapGenOn_idx_e2i(obj.BattOn_idx));
+            obj.BusIntId = full(mapBus_id_e2i(obj.BusId)); 
+            obj.GenOnIntIdx = full(mapGenOn_idx_e2i(obj.GenOnIdx));
+            obj.BattOnIntIdx = full(mapGenOn_idx_e2i(obj.BattOnIdx));
         end
         
         function obj = setDynamicSystem(obj, basecase_int, bus_id, branch_idx, genOn_idx, battOn_idx,...
@@ -114,16 +147,63 @@ classdef Zone < matlab.mixin.Copyable
             [obj.A, obj.Bc, obj.Bb, obj.Dg, obj.Dt, obj.Da] = dynamicSystem(basecase_int, bus_id, branch_idx, genOn_idx, battOn_idx,...
                 mapBus_id_e2i, mapGenOn_idx_e2i, sampling_time, batt_cst_power_reduc);
         end   
-            
-        function zone = setNumberOfElements(zone)
-            % Set the zone dimensions, i.e. the number of buses / branches
-            % / generators On / batteries On within the zone
-            zone.N_bus = size(zone.Bus_id,1);
-            zone.N_branch = size(zone.Branch_idx,1);
-            zone.N_genOn = size(zone.GenOn_idx,1);
-            zone.N_battOn = size(zone.BattOn_idx,1);
+
+        
+        %% Set property
+        function set.Duration(obj, duration)
+            arguments
+                obj
+                duration {mustBeInteger, mustBePositive}
+            end
+            obj.Duration = duration;
+            obj.updateNumberIteration;
         end
+        
+        function set.SamplingTime(obj, samplingTime)
+            arguments
+                obj
+                samplingTime {mustBeInteger, mustBePositive}
+            end
+            obj.SamplingTime = samplingTime;
+            obj.updateNumberIteration;
+        end
+          
+        function set.DelayBattSec(obj, delayBattSec)
+            obj.DelayBattSec = delayBattSec;
+            obj.updateDelayBatt;
+        end
+        
+        function set.DelayCurtSec(obj, delayCurtSec)
+            obj.DelayCurtSec = delayCurtSec;
+            obj.updateDelayCurt;
+        end
+        
     end
+    
+    methods(Access = private)
+            
+        function updateDelayBatt(obj)
+            obj.DelayBatt = ceil(obj.DelayBattSec/obj.SamplingTime);
+        end
+
+        function updateDelayCurt(obj)
+            obj.DelayCurt = ceil(obj.DelayCurtSec/obj.SamplingTime);
+        end
+
+        function updateNumberIteration(obj)
+            obj.NumberIteration = floor( obj.Duration / obj.SamplingTime);
+        end
+
+        function obj = updateNumberElement(obj)
+        % Set the zone dimensions: within the zone, number of buses/branches/generators On/batteries On
+        obj.NumberBus = size(obj.BusId,1);
+        obj.NumberBranch = size(obj.BranchIdx,1);
+        obj.NumberGenOn = size(obj.GenOnIdx,1);
+        obj.NumberBattOn = size(obj.BattOnIdx,1);
+        end
+        
+    end
+
 end
 
 
@@ -142,6 +222,14 @@ https://www.mathworks.com/help/matlab/matlab_oop/example-representing-structured
 https://www.mathworks.com/help/matlab/matlab_oop/comparing-handle-and-value-classes.html
 https://www.mathworks.com/help/matlab/matlab_oop/validate-property-values.html
 https://stackoverflow.com/questions/51294245/why-do-some-matlab-class-methods-require-apparently-unnecessary-output-argumen
+
+https://www.mathworks.com/help/matlab/matlab_oop/property-set-methods.html
+https://www.mathworks.com/help/matlab/matlab_oop/avoiding-property-initialization-order-dependency.html
+https://www.mathworks.com/help/matlab/matlab_oop/access-methods-for-dependent-properties.html
+https://www.mathworks.com/help/matlab/matlab_oop/property-attributes.html
+https://www.mathworks.com/help/matlab/matlab_oop/example-representing-structured-data.html#f2-85430
+
+https://www.mathworks.com/matlabcentral/answers/128905-how-to-efficiently-use-dependent-properties-if-dependence-is-computational-costly
 %}
 
 %{
