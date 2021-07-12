8129c4333fd4d1d771d304d45ced4107045e8410
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-07-12 14:22:32 +0200

    In Basecase class, addGenerator method done and tested

--- Basecase.m
+++ Basecase.m
@@ -11,8 +11,8 @@ classdef Basecase < handle
         end
         
         function addBus(obj, id, type, Pd, Qd, Gs, Bs, area, Vm, Va, baseKV, zone, maxVm, minVm)
-            % add a bus to the existing Matpowercase at the bottom of the list
-            % Input
+            % add a bus to the existing Matpowercase at the bottom of the 'bus' field
+            %% Input
             % All the needed values describing a branch according to MATPOWER
             % manual, see section Bus Data Format of CASEFORMAT, type "help
             % caseformat", or see arguments block in the source code
@@ -36,13 +36,32 @@ classdef Basecase < handle
                 [id, type, Pd, Qd, Gs, Bs, area, Vm, Va, baseKV, zone, maxVm, minVm];
         end
         
-        function addGenerator(obj)
-            % TODO
-        end
-        
-        
-        function addBattery(obj)
-            % TODO
+        function addGenerator(obj, bus_id, Pg_max, Pg_min, num, startup, shutdown, c3, c2, c1, c0)
+            % add a generator to the existing Matpowercase at the bottom of the 'gen' field.
+            % It is equivalent to Matpower's function 'addgen2mpc', but with default values.
+            
+            % CAUTIOUS! nr = number of rows in mpc.gen. gencost can either have nr rows or
+            % 2*nr, see Generator Cost Data Format. This function only treats the case with 'nr' rows
+            %% Input
+            % All the needed values describing a branch according to MATPOWER manual:
+            % or a subset not including the data for gencost
+            % see section Generator Data Format and Generator Cost Data of CASEFORMAT, type "help caseformat"
+            % or Matpower manual: Table B-2 Generator Data and Table B-4 Generator Cost data.
+            arguments
+                obj
+                bus_id (1,1) double {mustBeInteger, mustBePositive}
+                Pg_max (1,1) double {mustBeNonnegative}
+                Pg_min (1,1) double
+                num (1,1) double = 2
+                startup (1,1) double = 0
+                shutdown (1,1) double = 0
+                c3 (1,1) double = 0
+                c2 (1,1) double = 0
+                c1 (1,1) double = 0
+                c0 (1,1) double = 0
+            end
+            obj.Matpowercase.gencost(end+1,:) = [num startup shutdown c3 c2 c1 c0];
+            obj.Matpowercase.gen(end+1,:) = [bus_id 0 0 300 -300 1.025 100 1 Pg_max Pg_min zeros(1,11)];
         end
         
         function addBranch(obj)
--- BasecaseTest.m
+++ BasecaseTest.m
@@ -2,11 +2,22 @@ classdef BasecaseTest < matlab.unittest.TestCase
    
     methods(Test)
         
-        function case6468rte_modAddBus(testCase)
+        function case6468rte_modAddBusVG(testCase)
             basicCase = Basecase('case6468rte_mod');
-            basicCase.addBus(10000, 2,   0 ,      0,       0 ,  0,   1,  1.03864259,	-11.9454015,	63,	1,	1.07937,	0.952381);
-            expectedRowBus = [10000, 2,   0 ,      0,       0 ,  0,   1,  1.03864259,	-11.9454015,	63,	1,	1.07937,	0.952381];
+            bus_VG = 10000;
+            basicCase.addBus(bus_VG, 2,   0 ,      0,       0 ,  0,   1,  1.03864259,	-11.9454015,	63,	1,	1.07937,	0.952381);
+            expectedRowBus = [bus_VG, 2,   0 ,      0,       0 ,  0,   1,  1.03864259,	-11.9454015,	63,	1,	1.07937,	0.952381];
             testCase.verifyEqual(basicCase.Matpowercase.bus(end,:), expectedRowBus);
+        end
+        
+        function case6468rte_modAddGenOn2076(testCase)
+            basicCase = Basecase('case6468rte_mod');
+            bus = 2076;
+            maxGeneration = 66;
+            minGeneration = 0;
+            basicCase.addGenerator(bus, maxGeneration, minGeneration);
+            expectedRowGen = [bus 0 0 300 -300 1.025 100 1 maxGeneration minGeneration zeros(1,11)];
+            testCase.verifyEqual(basicCase.Matpowercase.gen(end,:), expectedRowGen);
         end
             
     end
