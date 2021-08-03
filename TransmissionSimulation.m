c9051c3f4264a95176494711e93ca98cd1bf7d73
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-08-03 22:20:25 +0200

    Set TransmissionSimulation.zoneName

--- TransmissionSimulation.m
+++ TransmissionSimulation.m
@@ -10,6 +10,14 @@ classdef TransmissionSimulation < handle
     methods 
         function obj = TransmissionSimulation(filenameSimulation)
             obj.simulationSetting = decodeJsonFile(filenameSimulation);
+            obj.setZoneName();
+        end
+        
+        function setZoneName(obj)
+            % the 'cell' data structure is used instead of 'matrix'.
+            % A matrix merges char arrays into a single char array, which
+            % would concatenate the zone names, which is not the desired behavior.
+           obj.zoneName = struct2cell(obj.simulationSetting.Zone); 
         end
     end
 end
