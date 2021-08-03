a29cd33c9b7f424e27083ccbd4339f61160e473b
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-08-03 18:52:55 +0200

    Add Zone

---
+++ Zone.m
@@ -0,0 +1,13 @@
+classdef Zone < handle
+   
+    properties
+       setting
+       transmissionSimulator
+       topology
+       telecomZone2Controller
+       telecomController2Zone
+       controller        
+    end
+    
+    
+end
