e708a6cfee08bdef240ece9c9cc238c5163db1a1
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-08-03 23:46:46 +0200

    Set Zone.timeSeries

--- Zone.m
+++ Zone.m
@@ -15,10 +15,11 @@ classdef Zone < handle
     end
     
     methods
-        function obj = Zone(name, electricalGrid)
+        function obj = Zone(name, electricalGrid, duration)
             obj.name = name;
             obj.setSetting();
             obj.setTopology(electricalGrid);
+            obj.setTimeSeries(duration);
         end
         
         function setSetting(obj)
@@ -33,6 +34,21 @@ classdef Zone < handle
         function setTopology(obj, electricalGrid)
            busId = obj.setting.busId;
            obj.topology = ZoneTopology(obj.name, busId, electricalGrid);
+        end
+        
+        function setTimeSeries(obj, duration)
+            filename = obj.setting.TimeSeries.filename;
+            
+            % Analyze the section of the json file regarding the TimeSeries setting
+            timeSeriesSetting = obj.setting.TimeSeries;
+            startPossibility = struct2cell(timeSeriesSetting.StartPossibilityForGeneratorInSeconds);
+            startSelected = timeSeriesSetting.startSelected;
+            start = startPossibility{startSelected};
+            controlCycle = obj.setting.controlCycle;
+            maxPowerGeneration = obj.topology.MaxPowerGeneration;
+            numberOfGenOn = obj.topology.NumberOfGen;
+            obj.timeSeries = TimeSeriesRenewableEnergy(...
+                filename, start, controlCycle, duration, maxPowerGeneration, numberOfGenOn);
         end
     end
 end
