a695973e3a6058b48a7d7cb050f15aa6ce5877aa
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-08-03 22:22:32 +0200

    Set TransmissionSimulation.numberOfZones

--- TransmissionSimulation.m
+++ TransmissionSimulation.m
@@ -4,10 +4,11 @@ classdef TransmissionSimulation < handle
        grid
        simulationSetting
        zoneName
+       numberOfZones
        zone
     end
    
-    methods 
+    methods
         function obj = TransmissionSimulation(filenameSimulation)
             obj.simulationSetting = decodeJsonFile(filenameSimulation);
             obj.setZoneName();
@@ -17,7 +18,11 @@ classdef TransmissionSimulation < handle
             % the 'cell' data structure is used instead of 'matrix'.
             % A matrix merges char arrays into a single char array, which
             % would concatenate the zone names, which is not the desired behavior.
-           obj.zoneName = struct2cell(obj.simulationSetting.Zone); 
+           obj.zoneName = struct2cell(obj.simulationSetting.Zone);
+        end
+        
+        function setNumberOfZones(obj)
+            obj.numberOfZones = size(obj.zoneName,1);
         end
     end
 end
