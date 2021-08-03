537144d3fd89c3e4283acd31911f67c784a6340e
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-08-03 22:19:20 +0200

    Set TransmissionSimulation.simulationSetting

--- TransmissionSimulation.m
+++ TransmissionSimulation.m
@@ -2,7 +2,14 @@ classdef TransmissionSimulation < handle
    
     properties
        grid
+       simulationSetting
        zoneName
        zone
+    end
+   
+    methods 
+        function obj = TransmissionSimulation(filenameSimulation)
+            obj.simulationSetting = decodeJsonFile(filenameSimulation);
+        end
     end
 end
