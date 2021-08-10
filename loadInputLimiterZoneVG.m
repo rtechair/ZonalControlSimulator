4dea49365be327fc039d7d9e484ea26e81514037
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-08-10 10:47:59 +0200

    Remove deprecated functions 'loadInputLimiterZone'

--- loadInputLimiterZoneVG.m
+++
@@ -1,4 +0,0 @@
-inputLimiterZoneVG.IncreaseCurtPercentEchelon = 0.1;
-inputLimiterZoneVG.DecreaseCurtPercentEchelon = 0.01;
-inputLimiterZoneVG.LowerThresholdPercent = 0.6;
-inputLimiterZoneVG.UpperThresholdPercent = 0.8;
--- loadInputLimiterZoneVTV.m
+++
@@ -1,4 +0,0 @@
-inputLimiterZoneVTV.IncreaseCurtPercentEchelon = 0.1;
-inputLimiterZoneVTV.DecreaseCurtPercentEchelon = 0.01;
-inputLimiterZoneVTV.LowerThresholdPercent = 0.6;
-inputLimiterZoneVTV.UpperThresholdPercent = 0.8;
