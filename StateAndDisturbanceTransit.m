c0beda90e15db3ffec376074e6c8939448be35da
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-08-12 17:35:24 +0200

    Update StateAndDisturbanceTransit readability

--- StateAndDisturbanceTransit.m
+++ StateAndDisturbanceTransit.m
@@ -1,22 +1,24 @@
 classdef StateAndDisturbanceTransit < handle
-    
+% Wrapper class of an object STATE + a value DISTURBANCE TRANSIT
+% such that the telecommunication, from the zone to the controller,
+% manipulates 1 object instead 1 object + 1 value.
    properties
       stateOfZone
       disturbanceTransit
    end
     
    methods
-       function obj = StateAndDisturbanceTransit(stateOfZone, disturbanceTransit)
-           obj.stateOfZone = stateOfZone;
-           obj.disturbanceTransit = disturbanceTransit;
+       function obj = StateAndDisturbanceTransit(objectStateOfZone, valueDisturbanceTransit)
+           obj.stateOfZone = objectStateOfZone;
+           obj.disturbanceTransit = valueDisturbanceTransit;
        end
        
-       function state = getStateOfZone(obj)
-           state = obj.stateOfZone;
+       function object = getStateOfZone(obj)
+           object = obj.stateOfZone;
        end
        
-       function distTransit = getDisturbanceTransit(obj)
-           distTransit = obj.disturbanceTransit;
+       function value = getDisturbanceTransit(obj)
+           value = obj.disturbanceTransit;
        end
    end
 end
