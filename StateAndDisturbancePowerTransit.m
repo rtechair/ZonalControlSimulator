299923370440c30763061b8e948202559b03c0dc
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-08-13 17:19:03 +0200

    Rename and update StateAndDistTransit, to become StateAndDisturbancePowerTransit

---
+++ StateAndDisturbancePowerTransit.m
@@ -0,0 +1,24 @@
+classdef StateAndDisturbancePowerTransit < handle
+% Wrapper class of an object STATE + a value DISTURBANCE POWER TRANSIT
+% such that the telecommunication, from the zone to the controller,
+% manipulates 1 object instead 1 object + 1 value.
+   properties (SetAccess = protected)
+      stateOfZone
+      disturbancePowerTransit
+   end
+    
+   methods
+       function obj = StateAndDisturbancePowerTransit(objectStateOfZone, valueDisturbancePowerTransit)
+           obj.stateOfZone = objectStateOfZone;
+           obj.disturbancePowerTransit = valueDisturbancePowerTransit;
+       end
+       
+       function object = getStateOfZone(obj)
+           object = obj.stateOfZone;
+       end
+       
+       function value = getDisturbancePowerTransit(obj)
+           value = obj.disturbancePowerTransit;
+       end
+   end
+end
