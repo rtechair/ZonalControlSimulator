d187c3c020e63aaddc44723e28f9073950fc1da9
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-08-13 15:51:20 +0200

    Clarify Limiter

--- Limiter.m
+++ Limiter.m
@@ -7,9 +7,8 @@ classdef Limiter < Controller
        controlBatt
        
        state
-       disturbancePowerTransit
-    end
-    
+       disturbancePowerTransit % unused by the Limiter
+    end    
     
     properties (SetAccess = immutable)
         branchFlowLimit
@@ -41,7 +40,7 @@ classdef Limiter < Controller
             obj.lowFlowThreshold = lowerThresholdPercent * branchFlowLimit;
             obj.highFlowThreshold = upperThresholdPercent * branchFlowLimit;
             
-            obj.doNotUseBatteries()
+            obj.doNotUseBatteries();
             
             
             obj.queueControlCurtPercent = zeros(1, delayCurtailment);
@@ -71,14 +70,14 @@ classdef Limiter < Controller
             if obj.isABranchOverHighFlowThreshold(branchFlowState) && obj.canCurtailmentIncrease()
                 obj.increaseCurtailment();
                 obj.updateFutureCurtailment();
-                obj.updateDelayedCurtControlPercentQueue();
+                obj.updateQueueControlCurtPercent();
             elseif obj.areAllBranchesUnderLowFlowThreshold(branchFlowState) && obj.canCurtailmentDecrease()
                 obj.decreaseCurtailment();
                 obj.updateFutureCurtailment();
-                obj.updateDelayedCurtControlPercentQueue();
+                obj.updateQueueControlCurtPercent();
             else
                 obj.doNotAlterCurtailment();
-                obj.updateDelayedCurtControlPercentQueue();
+                obj.updateQueueControlCurtPercent();
             end
         end
         
@@ -113,30 +112,37 @@ classdef Limiter < Controller
         end
                    
         
-        function isOverThreshold = isABranchOverHighFlowThreshold(obj, branchFlowState)
-           isOverThreshold = any( abs(branchFlowState) > obj.highFlowThreshold );
+        function boolean = isABranchOverHighFlowThreshold(obj, branchFlowState)
+           boolean = any( abs(branchFlowState) > obj.highFlowThreshold );
         end
         
-        function areAllUnderThreshold = areAllBranchesUnderLowFlowThreshold(obj, branchFlowState)
-            areAllUnderThreshold = all( abs(branchFlowState) < obj.lowFlowThreshold );
+        function boolean = areAllBranchesUnderLowFlowThreshold(obj, branchFlowState)
+            boolean = all( abs(branchFlowState) < obj.lowFlowThreshold );
         end
         
-        function canCurtIncrease = canCurtailmentIncrease(obj)
-            canCurtIncrease = (obj.futureStateCurtPercent + obj.increaseCurtPercentEchelon <= 1);
+        function boolean = canCurtailmentIncrease(obj)
+            boolean = (obj.futureStateCurtPercent + obj.increaseCurtPercentEchelon) <= 1;
         end
         
-        function canCurtDecrease = canCurtailmentDecrease(obj)
-            canCurtDecrease = (obj.futureStateCurtPercent + obj.decreaseCurtPercentEchelon >= 0);
+        function boolean = canCurtailmentDecrease(obj)
+            boolean = (obj.futureStateCurtPercent + obj.decreaseCurtPercentEchelon) >= 0;
         end
         
         function updateFutureCurtailment(obj)
             obj.futureStateCurtPercent = obj.futureStateCurtPercent + obj.controlCurtPercent;
         end
         
-        function updateDelayedCurtControlPercentQueue(obj)
-            OldestControlDropped = obj.queueControlCurtPercent(2:end);
-            newControlAdded = [OldestControlDropped obj.controlCurtPercent];
-            obj.queueControlCurtPercent = newControlAdded;
+        function updateQueueControlCurtPercent(obj)
+            obj.dropOldestControl();
+            obj.addNewControl();
+        end
+        
+        function dropOldestControl(obj)
+            obj.queueControlCurtPercent = obj.queueControlCurtPercent(2:end);
+        end
+        
+        function addNewControl(obj)
+            obj.queueControlCurtPercent = [obj.queueControlCurtPercent obj.controlCurtPercent];
         end
 
     end
