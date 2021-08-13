2635a10c6bfae8e0c1b8d6ba49faa0bbb1ac91df
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-08-13 14:53:57 +0200

    Change the constructor of ControlOfZone. Previously, a blank object is built, then its properties are set. Now the properties are set directly in the constructor

--- ControlOfZone.m
+++ ControlOfZone.m
@@ -8,24 +8,15 @@ classdef ControlOfZone < handle
    end
    
    methods
-       function obj = ControlOfZone(numberOfGenerators, numberOfBatteries)
-           % blank controls
-           obj.controlCurtailment = zeros(numberOfGenerators,1);
-           obj.controlBattery = zeros(numberOfBatteries,1);
+       function obj = ControlOfZone(controlCurtailment, controlBattery)
+           obj.controlCurtailment = controlCurtailment;
+           obj.controlBattery = controlBattery;
        end
        
        function modifyControl(obj, valueToAdd)
            % The telecommunication from the controller to the zone can modify the values
            obj.modifyControlCurtailment(valueToAdd);
            obj.modifyControlBattery(valueToAdd);
-       end
-       
-       function setControlCurtailment(obj, value)
-           obj.controlCurtailment = value;
-       end
-       
-       function setControlBattery(obj, value)
-           obj.controlBattery = value;
        end
    end
    
