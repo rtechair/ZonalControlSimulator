c31ce54eb2c9198d6d6221f5bdc958d3213cf0c3
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-08-17 14:11:39 +0200

    Update positions of ZoneTopology's methods

--- ZoneTopology.m
+++ ZoneTopology.m
@@ -41,6 +41,7 @@ classdef ZoneTopology < handle
             obj.setNumberOfElements();
         end
         
+        %% GETTER
         function value = getBusId(obj)
             value = obj.busId;
         end
@@ -81,31 +82,15 @@ classdef ZoneTopology < handle
             value = obj.branchBorderIdx;
         end
         
-        function genOffIdx = getGenOffIdx(obj, electricalGrid)
-            genOffIdx = electricalGrid.getGenOffIdx(obj.busId);
+        function value = getGenOffIdx(obj, electricalGrid)
+            value = electricalGrid.getGenOffIdx(obj.busId);
         end
          
-        function battOffIdx = getBattOffIdx(obj, electricalGrid)
-            battOffIdx = electricalGrid.getBattOffIdx(obj.busId);
+        function value = getBattOffIdx(obj, electricalGrid)
+            value = electricalGrid.getBattOffIdx(obj.busId);
         end
         
-        function G = getGraphStatic(obj, electricalGrid)
-            % Create the static graph of a zone defined by its branch indices in the
-            % basecase.
-        
-            insideAndBorderBranches = [obj.branchIdx; obj.branchBorderIdx];
-            [fromBus, toBus] = electricalGrid.getEndBuses(insideAndBorderBranches);
-            %{
-                %https://www.mathworks.com/help/matlab/ref/graph.html
-                Matlab's Graph object cares about the value / id of nodes, it will print as many nodes as
-                 the max id of nodes; e.g. if a node's number is 1000,
-                then Matlab assumes this is the 1000th node and will plot 1000 nodes, even
-                if it is the only node of the graph. Therefore, node's numbers are
-                converted into strings to avoid this strange behavior.
-            %}
-            G = graph(string(fromBus), string(toBus)); 
-        end
-        
+        %% GRAPH
         function P = plotLabeledGraph(obj, electricalGrid)
             % plot the static graph given as input while modifying the legend
             % 3 modifications in the plot:
@@ -156,7 +141,23 @@ classdef ZoneTopology < handle
                textNode = [num2str(obj.busId(bus, 1)), textGen, textBatt];
                labelnode(P, string(obj.busId(bus, 1)), textNode);
             end
-            
+        end
+        
+        function G = getGraphStatic(obj, electricalGrid)
+            % Create the static graph of a zone defined by its branch indices in the
+            % basecase.
+        
+            insideAndBorderBranches = [obj.branchIdx; obj.branchBorderIdx];
+            [fromBus, toBus] = electricalGrid.getEndBuses(insideAndBorderBranches);
+            %{
+                %https://www.mathworks.com/help/matlab/ref/graph.html
+                Matlab's Graph object cares about the value / id of nodes, it will print as many nodes as
+                 the max id of nodes; e.g. if a node's number is 1000,
+                then Matlab assumes this is the 1000th node and will plot 1000 nodes, even
+                if it is the only node of the graph. Therefore, node's numbers are
+                converted into strings to avoid this strange behavior.
+            %}
+            G = graph(string(fromBus), string(toBus));
         end
         
         function P = configurePlot(obj, P)
@@ -172,6 +173,7 @@ classdef ZoneTopology < handle
         
     end
     
+    %% SETTER
     methods (Access = protected)
        
         function setBranchesInZoneAndInBorder(obj, electricalGrid)
