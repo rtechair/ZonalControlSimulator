36392643bd752e00f6d2a16c4c446dba1d791dfe
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-08-25 11:04:31 +0200

    Ease the change of use case in the main

--- main.m
+++ main.m
@@ -2,5 +2,13 @@ transmission = TransmissionSimulation('simulation.json');
 
 transmission.runSimulation();
 
+isTopologyShown = false;
+isResultShown = true;
+
+if isTopologyShown
 transmission.plotZonesTopology();
+end
+
+if isResultShown
 transmission.plotZonesResult();
+end
