47539a6ed1ad30fa11a2aaf338a5c767de0e5326
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-08-30 10:04:39 +0200

    Manage the project with subrepositories

--- main.m
+++ main.m
@@ -1,3 +1,17 @@
+%{
+Add the project directory and all its subdirectories to the matlab search path.
+
+This action eases the folder management of the project: any source code can
+be in any folder of the project, matlab will find it.
+
+These additions are only temporary, they are removed when exiting matlab.
+
+main.m must remain in the root folder of the project, otherwise the following lines must be changed.
+%}
+filepath = fileparts(mfilename('fullpath'));
+addpath(genpath(filepath));
+
+%% RUN A SIMULATION
 transmission = TransmissionSimulation('simulation.json');
 
 transmission.runSimulation();
