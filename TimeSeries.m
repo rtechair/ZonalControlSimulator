c8e8bfdcc8ec1b399398b051ccacfa43e2d41e6f
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-09-27 13:35:48 +0200

    Changes on TimeSeries, to prepare for unit tests

--- TimeSeries.m
+++ TimeSeries.m
@@ -1,36 +1,41 @@
 classdef TimeSeries < handle
    
     properties (SetAccess = protected)
-        windChargingRate
+        chargingRate
+        offsetChargingRate
         ProfilePowerAvailable
         ProfileDisturbancePowerAvailable
         step
     end
     
     properties (SetAccess = immutable)
-        startGen
+        genStart
         numberOfIterations
         maxPowerGeneration
         numberOfGen
     end
     
     methods
-        
-        function obj = TimeSeries(filenameWindChargingRate, ...
-                startGenInSeconds, controlCycle, ...
-                durationSimulation, maxPowerGeneration, numberOfGen)
-            obj.startGen = startGenInSeconds;
-            obj.numberOfIterations = floor(durationSimulation / controlCycle);
+        % TODO, adapt the build function too
+        function obj = TimeSeries(chargingRateFilename, windowSimulation, ...
+                durationSimulation, maxPowerGeneration, genStart)
+            obj.genStart = genStart;
+            obj.numberOfIterations = floor(durationSimulation / windowSimulation);
             obj.maxPowerGeneration = maxPowerGeneration;
-            obj.numberOfGen = numberOfGen;
+            
+            if size(maxPowerGeneration,1) ~= size(genStart,1)
+                error('sizes of maxPowerGeneration and genStart are different')
+            end
+            obj.numberOfGen = size(maxPowerGeneration,1);
             
             % step starts at 0 because of initialization, later updated to 1 to start the simulation
-            obj.step = 0;
+            obj.step = 0; % TODO USELESS, it is a design flow in the simulation algorithm. TODO: later remove this part
             
-            obj.setWindChargingRate(filenameWindChargingRate);
-            obj.checkInitialIterationCorrectness(controlCycle)
+            obj.setChargingRate(chargingRateFilename);
             
-            obj.setProfilePowerAvailable(controlCycle);
+            obj.checkInitialIterationCorrectness(windowSimulation)
+            
+            obj.setProfilePowerAvailable(windowSimulation);
             obj.setProfileDisturbancePowerAvailable();
         end
         
@@ -38,6 +43,8 @@ classdef TimeSeries < handle
             value = obj.ProfilePowerAvailable(:,1);
         end
         
+        % TODO: when the telecommunication is removed, this method needs to
+        % change
         function object = getDisturbancePowerAvailable(obj)
             % The telecommunication needs an object, not a matrix of the
             % value. Thus, the method returns an object wrapping the value
@@ -46,7 +53,7 @@ classdef TimeSeries < handle
             object = DisturbancePowerAvailable(value);
         end
         
-       function prepareForNextStep(obj)
+       function goToNextStep(obj)
             obj.step = obj.step + 1;
        end 
         
@@ -54,48 +61,51 @@ classdef TimeSeries < handle
     
     methods (Access = protected)
        
-        
-        function setWindChargingRate(obj, filenameWindChargingRate)
+        function setChargingRate(obj, filenameChargingRate)
            % the apostrophe is to obtain a row vector, such that columns represent the time
-           obj.windChargingRate = table2array(readtable(filenameWindChargingRate))';
+           obj.chargingRate = table2array(readtable(filenameChargingRate))';
         end
         
-       function setProfilePowerAvailable(obj, controlCycle)
-           genWindRate = zeros(obj.numberOfGen, obj.numberOfIterations + 1);
-           for i = 1:obj.numberOfGen
-               start = obj.startGen(i);
-               last = start + obj.numberOfIterations*controlCycle;
-               range = start : controlCycle : last;
-               genWindRate(i,:) = obj.windChargingRate(1, range);
+        function setOffsetChargingRate(obj)
+            obj.offsetChargingRate = NaN(obj.numberOfGen, obj.numberOfIterations+1);
+            for i = 1:obj.numberOfGen
+               start = obj.genStart(i);
+               last = start + obj.numberOfIterations*windowSimulation;
+               range = start : windowSimulation : last;
+               obj.offsetChargingRate(i,:) = obj.chargingRate(1, range);
            end
-           obj.ProfilePowerAvailable = obj.maxPowerGeneration .* genWindRate;
+        end
+        
+       function setProfilePowerAvailable(obj)
+           obj.ProfilePowerAvailable = obj.maxPowerGeneration .* obj.offsetChargingRate;
        end
        
        function setProfileDisturbancePowerAvailable(obj)
-           obj.ProfileDisturbancePowerAvailable = zeros(obj.numberOfGen, obj.numberOfIterations);
+           obj.ProfileDisturbancePowerAvailable = NaN(obj.numberOfGen, obj.numberOfIterations);
            for time = 1:obj.numberOfIterations
                obj.ProfileDisturbancePowerAvailable(:,time) = obj.ProfilePowerAvailable(:, time+1) ...
                    - obj.ProfilePowerAvailable(:, time);
            end
        end
        
-       function checkInitialIterationCorrectness(obj, controlCycle)
-           sampleDuration = size(obj.windChargingRate,2);
+       function checkInitialIterationCorrectness(obj, windowSimulation)
+           sampleDuration = size(obj.chargingRate,2);
            % the following '-1' is due to the initialization step
-           maxStartingTimeForGen = sampleDuration - 1 - controlCycle*obj.numberOfIterations;
-           isThereAnyStartOfGenTooLate = any(obj.startGen > maxStartingTimeForGen);
+           maxStartingTimeForGen = sampleDuration - 1 - windowSimulation*obj.numberOfIterations;
+           isThereAnyStartOfGenTooLate = any(obj.genStart > maxStartingTimeForGen);
            if isThereAnyStartOfGenTooLate
                 obj.errorStartingIterationExceedsMax(maxStartingTimeForGen)
            end
        end
        
        function errorStartingIterationExceedsMax(obj, upperBound)
-           message = ['the starting iterations chosen for wind time series of the generators exceeds ' ...
+           message = ['the starting iterations chosen for time series of the generators exceeds ' ...
             'the max discrete range, check in the JSON file, that the selected starts for generators are < ' ...
             num2str(upperBound) ', in the load data zone script'];
         error(message)
        end
        
+       % TODO: the name should be changed
        function value = getDisturbancePowerAvailableValue(obj)
            value = obj.ProfileDisturbancePowerAvailable(:,obj.step);
        end
