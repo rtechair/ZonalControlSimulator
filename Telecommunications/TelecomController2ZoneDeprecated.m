b6410c207ef085fbe732f1d537bf73c30e1bab74
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-10-06 14:16:54 +0200

    Refactor the exchange of info from the controller to the zone

---
+++ Telecommunications/TelecomController2ZoneDeprecated.m
@@ -0,0 +1,23 @@
+classdef TelecomController2Zone < Telecommunication
+    
+    methods
+        function obj = TelecomController2Zone(numberOfGen, numberOfBatt, delayTelecom)
+            obj.delay = delayTelecom;
+            blankControlCurtailment = zeros(numberOfGen,1);
+            blankControlBattery = zeros(numberOfBatt,1);
+            blankControls(1:delayTelecom) = ControlOfZone(blankControlCurtailment, blankControlBattery);
+            obj.queueData = blankControls;
+        end
+    end
+    
+    methods (Access = protected)
+        function receive(obj, emitter)
+            obj.queueData(end+1) = emitter.getControl();
+        end
+        
+        function send(obj, receiver)
+            receiver.receiveControl(obj.queueData(1));
+        end
+    end
+    
+end
