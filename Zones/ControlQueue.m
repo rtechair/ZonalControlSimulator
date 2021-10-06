d1e4aa302f63b534ba5bc77b9a968fec85af8f24
Guillaume Ganet--Lepage <g.ganet.lepage@gmail.com>
2021-10-06 15:53:40 +0200

    rename arguments and properties

--- Zones/ControlQueue.m
+++ Zones/ControlQueue.m
@@ -3,34 +3,34 @@ classdef ControlQueue < handle
     properties (SetAccess = protected)
         curtControlQueue
         battControlQueue
-        curtDelay
-        battDelay
+        delayCurt
+        delayBatt
     end
     
     methods
         function obj = ControlQueue(...
-                numberOfGenOn, curtDelay, numberOfBattOn, battDelay)
-            obj.curtDelay = curtDelay;
-            obj.battDelay = battDelay;
-            obj.curtControlQueue = [zeros(numberOfGenOn, curtDelay) NaN(numberOfGenOn, 1)];
-            obj.battControlQueue = [zeros(numberOfBattOn, battDelay) NaN(numberOfBattOn, 1)];
+                numberOfGenOn, delayCurt, numberOfBattOn, delayBatt)
+            obj.delayCurt = delayCurt;
+            obj.delayBatt = delayBatt;
+            obj.curtControlQueue = [zeros(numberOfGenOn, delayCurt) NaN(numberOfGenOn, 1)];
+            obj.battControlQueue = [zeros(numberOfBattOn, delayBatt) NaN(numberOfBattOn, 1)];
         end
         
         function enqueue(obj, controlOfZone)
-            obj.curtControlQueue(:,obj.curtDelay+1) = controlOfZone.getControlCurtailment();
-            obj.battControlQueue(:,obj.battDelay+1) = controlOfZone.getControlBattery();
+            obj.curtControlQueue(:,obj.delayCurt+1) = controlOfZone.getControlCurtailment();
+            obj.battControlQueue(:,obj.delayBatt+1) = controlOfZone.getControlBattery();
         end
         
-        function controlToApply = dequeue(obj)
-            controlCurtToApply = obj.curtControlQueue(:,1);
-            controlBattToApply = obj.battControlQueue(:,1);
-            controlToApply = ControlOfZone(controlCurtToApply, controlBattToApply);
+        function control = dequeue(obj)
+            controlCurt = obj.curtControlQueue(:,1);
+            controlBatt = obj.battControlQueue(:,1);
+            control = ControlOfZone(controlCurt, controlBatt);
             obj.removeFirst();
         end
         
         function removeFirst(obj)
-            obj.curtControlQueue = obj.curtControlQueue(:, 2:obj.curtDelay+1);
-            obj.battControlQueue = obj.battControlQueue(:, 2:obj.battDelay+1);
+            obj.curtControlQueue = obj.curtControlQueue(:, 2:obj.delayCurt+1);
+            obj.battControlQueue = obj.battControlQueue(:, 2:obj.delayBatt+1);
         end
     end
 end
